6342282a4c21fd2a207646bd0699ee792da7d704
zacharyellison <zach.r.ellison@gmail.com>
2025-11-23 10:44:06 -0500

    from default

---
+++ cache/skillsville.wlp/js/_editor_index.js
@@ -0,0 +1,4 @@
+import * as __wonderlandengine_components from '@wonderlandengine/components';
+_registerEditor(__wonderlandengine_components);
+import * as _C__Users_zachr_code_Skillsville_js_button_js from 'C:/Users/zachr/code/Skillsville/js/button.js';
+_registerEditor(_C__Users_zachr_code_Skillsville_js_button_js);
